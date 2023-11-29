568073c14d3292745069fcc6a7faa9794f8d84f9
waliradim <waliradim@gmail.com>
2023-11-29 23:36:59 +0600

    asterisk

--- asterisk.js
+++ asterisk.js
@@ -1,4 +1,11 @@
 //problem - 14
+// Write a program that prints a triangle asterisk (*). The number of rows in the triangle is determined by the user's input. Each row should contain a  string where the characters are repeated in a mirrored pattern.
+// For example, if the user inputs 5, the program should print:
+//       *
+//     ***
+//    ****
+//   *****
+//  ******
 
 const asterisk = (inputValue) => {
   let space = "";
