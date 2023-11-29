ad8b8e435c2cde7e64b707a225bafe819b653996
waliradim <waliradim@gmail.com>
2023-11-29 23:34:49 +0600

    getMaxProduct

--- getMaxProduct.js
+++ getMaxProduct.js
@@ -1,4 +1,7 @@
 //problem - 12
+// Write a function called "getMaxProduct" that takes an array of integers as input and returns the maximum product that can be obtained by multiplying any two distinct elements from the array.
+// Sample Input: [2, 3, 5, 6, 7]
+// Sample Output: 42 (obtained by multiplying 6 and 7)
 
 getMaxProduct = (arry) => {
   if (Array.isArray(arry)) {
