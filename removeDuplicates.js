8e6f847ff8e84aac73490ecbdd136fb7d4a055c9
waliradim <waliradim@gmail.com>
2023-11-29 23:31:20 +0600

    removeDuplicates

--- removeDuplicates.js
+++ removeDuplicates.js
@@ -1,4 +1,11 @@
 //problem - 7
+// Suppose you are building a student database for your class that will track student’s names and their marks. Your class has 40 students but now you are seeing that there are 41 entries in your database so you decide to check the database. And you find out that you have mistakenly uploaded a student’s name twice.
+// Task
+//  Write a `removeDuplicates` function that takes in an array of names and returns a new array with any duplicates removed.
+// Sample Input:
+// Student_names = [‘Zara’, ‘Sadia’ , ‘Mahin’ , ‘Adnan’ , ‘Maisha’, ‘Adnan’, ‘Faiyaz’]
+// Sample Output :
+// Student_names = [‘Zara’, ‘Sadia’ , ‘Mahin’ , ‘Adnan’ , ‘Maisha’, ‘Faiyaz’]
 
 const removeDuplicates = (arry) => {
   if (Array.isArray(arry)) {
