37f8f36af40e33eb63220a36fe277c28824ca3ab
waliradim <waliradim@gmail.com>
2023-11-29 23:33:55 +0600

    findMissingNumber

--- findMissingNumber.js
+++ findMissingNumber.js
@@ -1,4 +1,8 @@
 //problem -11
+// Write a function called "findMissingNumber" that takes an array of consecutive integers from 1 to n, with one number missing, and returns the missing number.
+// For example:
+// Input: [1, 2, 3, 5, 6, 7, 8]
+// Output: 4
 
 const findMissingNumber = (arry) => {
   if (Array.isArray(arry)) {
