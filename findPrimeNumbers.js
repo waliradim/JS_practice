a3af377f2b6bb7f2998af062479dfd6b5600552e
waliradim <waliradim@gmail.com>
2023-11-29 23:36:14 +0600

    findPrimeNumbers

--- findPrimeNumbers.js
+++ findPrimeNumbers.js
@@ -1,4 +1,8 @@
 //problem -13
+// Write a function called "findPrimeNumbers" that takes an array of positive integers as input and returns a new array containing only the prime numbers from the original array.
+// For example:
+// Input: [2, 3, 4, 5, 6, 7, 8, 9, 10]
+// Output: [2, 3, 5, 7]
 
 findPrimeNumbers = (arry) => {
   if (Array.isArray(arry)) {
