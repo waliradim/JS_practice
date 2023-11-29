a6157aa374f18a20a9c2b7e6a5ff9516dfdf5f4f
waliradim <waliradim@gmail.com>
2023-11-29 23:28:00 +0600

    isPalindrome

--- Palindrome.js
+++ Palindrome.js
@@ -1,4 +1,10 @@
 //problem - 4
+// Write a function isPalindrome that takes in a string and returns true if the string is a palindrome (reads the same forwards and backwards) and false otherwise.
+// Example Input/Output:
+// isPalindrome("racecar") should return true
+// isPalindrome("hello") should return false
+// isPalindrome("rotator") should return true
+// isPalindrome("peep") should return true
 
 isPalindrome = (vlu) => {
   if (typeof vlu === "string") {
