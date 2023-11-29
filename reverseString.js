9bd0d2187e76a5f12222fad05b77180f38b11b2d
waliradim <waliradim@gmail.com>
2023-11-29 23:26:55 +0600

    reverseString

--- reverseString.js
+++ reverseString.js
@@ -1,4 +1,9 @@
 //problem - 3
+// Write a function called `reverseString` that takes a string as input and returns the reverse of that string. Your function should not use the built-in `reverse()` method.
+// Example Input/Output:
+// - reverseString('hello') should return 'olleh'
+// - reverseString('racecar') should return 'racecar'
+// - reverseString('12345') should return '54321'
 
 reverseString = (vlu) => {
   if (typeof vlu === "string") {
