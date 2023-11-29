31a82d33c0511f681722bd3a920b943473f696d6
waliradim <waliradim@gmail.com>
2023-11-29 23:33:10 +0600

    getPositiveNumbers

--- getPositiveNumbers.js
+++ getPositiveNumbers.js
@@ -1,4 +1,7 @@
 //problem - 10
+// Given an array exists that has integers, write a function called "getPositiveNumbers" that takes the entire array as input and returns a new array containing only the positive numbers from the original array.
+// Sample Input : [2, -5, 10, -3, 8, -1, 0, 7]
+// Sample Output: [2, 10, 8, 7]
 
 const getPositiveNumbers = (arry) => {
   if (Array.isArray(arry)) {
