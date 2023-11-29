ef83ec0b09e795bc0246a352198dbcc96e3ae0bc
waliradim <waliradim@gmail.com>
2023-11-29 23:32:22 +0600

    parseString

--- parseString.js
+++ parseString.js
@@ -1,4 +1,13 @@
 //problem - 9
+// Write a Javascript Program that takes String as a parameter and checks if the parameters are number or text. If the parameters are numbers then it will return a summation of the numbers. If the parameters are not numbers then it will generate a text by concatenating the strings.
+// Sample Input:
+// parseString(“21” , “24’ , “40”)
+// parseString(“Hello” , “Alpha”)
+// parseString(“Summer” , “2022”)
+// Sample Output:
+// 85
+// Hello Alpha
+// Summer 2022
 
 const parseString = (...vlu) => {
   let value = vlu;
